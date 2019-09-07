55c078f58afa76cf04e65eef48645e1c3d16bf6d
root <root@localhost.localdomain>
2019-09-07 20:45:02 +0530

    updated flask app

--- tool/app.py
+++ tool/app.py
@@ -1,6 +1,5 @@
-from flask import Flask, render_template, request
-import requests
-import json
+from flask import Flask, render_template, request, jsonify
+import cms_finder
 
 app = Flask(__name__)
 
@@ -8,16 +7,11 @@ app = Flask(__name__)
 def index():
    return render_template('index.html')
 
-@app.route('/result',methods = ['GET', 'POST'])
+@app.route('/',methods = ['GET', 'POST'])
 def basicInfo():
-	key='4ac0c57c5f2ec2b1bf7e0bfaaef52e91f44eaecbd5a8e640cd69435bc95ab96b856aef'
-	param=request.form['website']
-	url = 'https://whatcms.org/APIEndpoint/Technology?key='+key+'&url='+param
-	# r = requests.get(url)
-	# var= r.json()
-	test='{"request":"https:\/\/whatcms.org\/APIEndpoint\/Technology?key=4ac0c57c5f2ec2b1bf7e0bfaaef52e91f44eaecbd5a8e640cd69435bc95ab96b856aef&url=http:\/\/adgitmdelhi.ac.in\/","request_web":"https:\/\/whatcms.org\/?s=http%3A%2F%2Fadgitmdelhi.ac.in%2F","result":{"code":200,"msg":"Success"},"results":[{"categories":["CMS","Blog"],"name":"WordPress","url":"https:\/\/whatcms.org\/c\/WordPress","version":"5.2.2"},{"categories":["Programming Language"],"name":"PHP","version":""},{"categories":["Database"],"name":"MySQL","version":""},{"categories":["Web Server"],"name":"Nginx","version":"1.17.3"}]}'
-	var = json.loads(test) 
-	return (var['results'][0]['version'] + '\n' + var['results'][0]['name'])
+	param = request.form['website']
+	l = cms_finder.cms_find(param)
+	return jsonify(results = l)
 
 if __name__ == '__main__':
    app.run(debug = True)
--- tool/cms_finder.py
+++ tool/cms_finder.py
@@ -2,14 +2,14 @@ import requests
 import json 
 ###!---- Finding Out the Name and the Version ----!
 key='4ac0c57c5f2ec2b1bf7e0bfaaef52e91f44eaecbd5a8e640cd69435bc95ab96b856aef'
-def cms_find():
+def cms_find(param):
 	List=[]
-	param='http://adgitmdelhi.ac.in/'
+	#param='http://adgitmdelhi.ac.in/'
 	url = 'https://whatcms.org/APIEndpoint/Technology?key='+key+'&url='+param
 	r = requests.get(url)
 	var= r.json()
-	#test='{"request":"https:\/\/whatcms.org\/APIEndpoint\/Technology?key=4ac0c57c5f2ec2b1bf7e0bfaaef52e91f44eaecbd5a8e640cd69435bc95ab96b856aef&url=http:\/\/adgitmdelhi.ac.in\/","request_web":"https:\/\/whatcms.org\/?s=http%3A%2F%2Fadgitmdelhi.ac.in%2F","result":{"code":200,"msg":"Success"},"results":[{"categories":["CMS","Blog"],"name":"WordPress","url":"https:\/\/whatcms.org\/c\/WordPress","version":"5.2.2"},{"categories":["Programming Language"],"name":"PHP","version":""},{"categories":["Database"],"name":"MySQL","version":""},{"categories":["Web Server"],"name":"Nginx","version":"1.17.3"}]}'
-	#var = json.loads(test) 
+	test='{"request":"https:\/\/whatcms.org\/APIEndpoint\/Technology?key=4ac0c57c5f2ec2b1bf7e0bfaaef52e91f44eaecbd5a8e640cd69435bc95ab96b856aef&url=http:\/\/adgitmdelhi.ac.in\/","request_web":"https:\/\/whatcms.org\/?s=http%3A%2F%2Fadgitmdelhi.ac.in%2F","result":{"code":200,"msg":"Success"},"results":[{"categories":["CMS","Blog"],"name":"WordPress","url":"https:\/\/whatcms.org\/c\/WordPress","version":"5.2.2"},{"categories":["Programming Language"],"name":"PHP","version":""},{"categories":["Database"],"name":"MySQL","version":""},{"categories":["Web Server"],"name":"Nginx","version":"1.17.3"}]}'
+	var = json.loads(test) 
 	List.append(var['results'][0]['version'])
 	List.append(var['results'][0]['name'])
 	return List
