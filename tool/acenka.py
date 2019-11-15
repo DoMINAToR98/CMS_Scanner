4ff3937e2cf3a7a48036fbed67ff621add2709b0
root <root@localhost.localdomain>
2019-11-15 16:18:21 +0530

    Updated app.py to include multiple threads Added nmap support
    TO-DO: Change CSS to Old One

--- tool/acenka.py
+++ tool/acenka.py
@@ -1,38 +1,49 @@
 import subprocess
 import requests
-import json 
+import json
+import socket
+from urlparse import urlparse
+
 ###!---- Finding Out the Name and the Version ----!
 key='4ac0c57c5f2ec2b1bf7e0bfaaef52e91f44eaecbd5a8e640cd69435bc95ab96b856aef'
-#param='http://adgitmdelhi.ac.in/' --> wp
+#param='http://adgitmdelhi.ac.in/'  --> wp
 #param'https://unric.org/en/' --> joomla
 #param='https://www.rutgers.edu/' --> drupal
-parameter=''
+
 def cms_find(param):
 	List=[]
 	url = 'https://whatcms.org/APIEndpoint/Technology?key='+key+'&url='+param
 	r = requests.get(url)
 	var= r.json()
-	parameter=param
 	dummy_to_save_requests='{"request":"https:\/\/whatcms.org\/APIEndpoint\/Technology?key=4ac0c57c5f2ec2b1bf7e0bfaaef52e91f44eaecbd5a8e640cd69435bc95ab96b856aef&url=http:\/\/adgitmdelhi.ac.in\/","request_web":"https:\/\/whatcms.org\/?s=http%3A%2F%2Fadgitmdelhi.ac.in%2F","result":{"code":200,"msg":"Success"},"results":[{"categories":["CMS","Blog"],"name":"Joomla","url":"https:\/\/whatcms.org\/c\/WordPress","version":"5.2.2"},{"categories":["Programming Language"],"name":"PHP","version":""},{"categories":["Database"],"name":"MySQL","version":""},{"categories":["Web Server"],"name":"Nginx","version":"1.17.3"}]}'
-	#var = json.loads(dummy_to_save_requests) 
-	List.append(var['results'][0]['version'])
-	List.append(var['results'][0]['name'])
+	#var = json.loads(dummy_to_save_requests)
+	List.append(var['results'][0]['version'].encode("utf-8"))
+	List.append(var['results'][0]['name'].encode("utf-8"))
 	return List
 
-def scan(cmslist):
+def ip_find(param):
+	uparse = urlparse(param)
+	ip = socket.gethostbyname(uparse[1])
+	return ip
+
+def port_find(param):
+	subprocess.call(["nmap","-A", "-oA","nmap_scan",param])
+
+
+def scan(cmslist,param):
 	target=[]
 	target.append(cmslist)
 	###!---- Generating the report in Text Format ----!
 	if(target[0][1]=="WordPress"):
 		print "!---Starting WordPress Scan---!"
-		subprocess.call("wpscan --url "+parameter+" --no-banner --detection-mode aggressive -o target_report.txt".split()) 
+		subprocess.call("wpscan --url "+ param +" --no-banner --ignore-maine-redirect --detection-mode aggressive -o target_reportwp.txt".split())
 	elif(target[0][1]=="Joomla"):
 		print "!---Starting Joomla Scan---!"
-		subprocess.call(("joomscan -u "+parameter+" > target_report.txt").split())
+		subprocess.call(("joomscan -u "+param+" > target_reportJoomla.txt").split())
 	elif(target[0][1]=="Drupal"):
 		print "!---Starting Drupal Scan---!"
-		subprocess.call(("droopescan scan drupal -u "+parameter+" > target_report.txt").split())
-	else: 
+		subprocess.call(("droopescan scan drupal -u "+param+" > target_reportDrupal.txt").split())
+	else:
 		print "No support for "+target[0][1]+" yet."
 
 ###!---- Beautify the text file for pdf generation ----!
